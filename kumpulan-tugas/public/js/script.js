d6b1ad98a6e305722d150be326d2e85f240f7ee8
Rama Fajar Fadhilllah <78191852+ramarff@users.noreply.github.com>
2022-05-09 09:22:13 +0700

    Add files via upload

---
+++ kumpulan-tugas/public/js/script.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    $('.tombolTambahData').on('click', function() {
+        $('#formModalLabel').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+        $('#nama').val('');
+        $('#nim').val('');
+        $('#email').val('');
+        $('#jurusan').val('Pilih Jurusan');
+        $('#id').val('');
+    });
+
+
+    $('.tampilModalUbah').on('click', function() {
+        
+        $('#formModalLabel').html('Ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal form').attr('action', 'http://localhost/kuliah/mvc/public/mahasiswa/ubah');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost/kuliah/mvc/public/mahasiswa/getubah',
+            data: {id : id},
+            method: 'post',
+            dataType: 'json',
+            success: function(data) {
+                $('#nama').val(data.nama);
+                $('#nim').val(data.nim);
+                $('#email').val(data.email);
+                $('#jurusan').val(data.jurusan);
+                $('#id').val(data.id);
+            }
+        });
+        
+    });
+
+});
